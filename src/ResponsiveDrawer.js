05a1844b7f48e5e7f89830aed2763514179c864a
Frank Mazzarella <mazzarella.frank@gmail.com>
2020-03-25 23:36:46 -0400

    version bump to 1.0.1

--- src/ResponsiveDrawer.js
+++ src/ResponsiveDrawer.js
@@ -103,7 +103,7 @@ function ResponsiveDrawer(props) {
       <List className={classes.version}>
         <ListItem key={'Version'}>
           <ListItemIcon><InfoIcon /></ListItemIcon>
-          <ListItemText primary={'Version 1.0.0'} />
+          <ListItemText primary={'Version 1.0.1'} />
         </ListItem>
       </List>
     </div>
