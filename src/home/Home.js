98d3c5b319d6739c2c4a7669030a633c26431cf4
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-01-01 13:33:19 -0500

    refactor to keep lines under 120 chars

--- src/home/Home.js
+++ src/home/Home.js
@@ -37,7 +37,11 @@ export default function Home() {
       </Grid>
       <Grid item sm={10} lg={10}>
         <Typography className={classes.story}>
-        2020 has come to an end.  The crazy year ended fittingly, with a crazy tournament.  The 2020 New Year's Eve Open saw Free Will finally join the ranks of the elite, winning the tournament in their first ever sweep.  They made it look easy, dispatching their opponents without much of a fight.  Don't let that distract you from the fact that Fire Breathing Rubber Duckies eliminated Rock of Gibraltar!  While S.A.M. still maintains first place, Free Will is finally within striking distance.  Who knows what 2021 will hold.
+          2020 has come to an end. The crazy year ended fittingly, with a crazy tournament. The 2020 New Year's Eve
+          Open saw Free Will finally join the ranks of the elite, winning the tournament in their first ever sweep.
+          They made it look easy, dispatching their opponents without much of a fight. Don't let that distract you from
+          the fact that Fire Breathing Rubber Duckies eliminated Rock of Gibraltar! While S.A.M. still maintains first
+          place, Free Will is finally within striking distance. Who knows what 2021 will hold.
         </Typography>
       </Grid>
     </Grid>
