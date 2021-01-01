fce0096f584b94ed9e5c9c8e7ca7e737f836f81b
samfumanti <samuelfumanti@gmail.com>
2021-01-01 11:35:52 -0500

    New Year's Eve Open Update

--- home/Home.js
+++ home/Home.js
@@ -33,11 +33,11 @@ export default function Home() {
         <br /><br />
       </Grid>
       <Grid item sm={10} lg={10}>
-        <h1 className={classes.header}>Play It Again, S.A.M.</h1>
+        <h1 className={classes.header}>New Year, New Champs</h1>
       </Grid>
       <Grid item sm={10} lg={10}>
         <Typography className={classes.story}>
-        Christmas Eve came and went, and with it was another memorable tournament.  Rock of Gibraltar flashed back to their prime, beating Free Will in Round 1.  They followed that up with a commanding 9-5 lead against S.A.M.  For the second time in history, though, S.A.M. showed why they're #1 with 5 straight points to stay out of the losers' bracket.  Free Will and Rock of Gibraltar met again, this time in an elimination game, and the girls fed off S.A.M.'s energy to get back to the championship game.  The champs were clearly at their best, though, and swept a tournament for the first time since February, claiming this year's Christmas Eve Clash.
+        2020 has come to an end.  The crazy year ended fittingly, with a crazy tournament.  The 2020 New Year's Eve Open saw Free Will finally join the ranks of the elite, winning the tournament in their first ever sweep.  They made it look easy, dispatching their opponents without much of a fight.  Don't let that distract you from the fact that Fire Breathing Rubber Duckies eliminated Rock of Gibraltar!  While S.A.M. still maintains first place, Free Will is finally within striking distance.  Who knows what 2021 will hold.
         </Typography>
       </Grid>
     </Grid>
