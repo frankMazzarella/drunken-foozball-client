6a29f926eabe0d14f9ece386bb74a8662bf1dc8d
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-01-10 23:10:25 -0500

    adding snackbar for edit rules messages

--- src/admin/EditRules.js
+++ src/admin/EditRules.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, Snackbar, SnackbarContent } from '@material-ui/core';
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-twilight";
@@ -11,9 +11,10 @@ import FirebaseService from '../services/Firebase.serivce';
 export default class EditRules extends Component {
   constructor(props) {
     super(props);
-    this.state = { editorValue: '' };
+    this.state = { editorValue: '', showSnackbar: false, snackbarMessage: '' };
     this.handleSaveClicked = this.handleSaveClicked.bind(this);
     this.handleEditorValueChange = this.handleEditorValueChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
     this.rulesRef = FirebaseService.getDatabaseRef('rules');
   }
 
@@ -27,14 +28,15 @@ export default class EditRules extends Component {
     try {
       JSON.parse(this.state.editorValue);
     } catch (error) {
-      return console.error('shit cant do the json');
+      this.openSnackbar('ERROR: Update failed because JSON is malformed!');
+      return;
     }
     this.rulesRef.set(btoa(this.state.editorValue), (error) => {
       if (error) {
-        console.log('a wild error has appeared');
         console.error(error);
+        this.openSnackbar(error.message);
       } else {
-        console.log('done');
+        this.openSnackbar('Update was successful');
       }
     });
   }
@@ -43,24 +45,44 @@ export default class EditRules extends Component {
     this.setState({ editorValue: newValue });
   }
 
+  openSnackbar(message) {
+    this.setState({ snackbarMessage: message });
+    this.setState({ showSnackbar: true });
+  }
+
+  closeSnackbar(event, reason) {
+    if (reason === 'clickaway') return;
+    this.setState({ showSnackbar: false });
+  }
+
   render() {
     return (
-      <Grid container direction="column" alignItems="center">
-        <Grid item xs={12}>
-          <Button variant="contained" component={Link} to="/admin">Go Back</Button>
-          <Button variant="contained" onClick={this.handleSaveClicked} color="primary">Save</Button>
+      <>
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Button variant="contained" component={Link} to="/admin">Go Back</Button>
+            <Button variant="contained" onClick={this.handleSaveClicked} color="primary">Save</Button>
+          </Grid>
+          <AceEditor
+            defaultValue="Loading..."
+            value={this.state.editorValue}
+            onChange={this.handleEditorValueChange}
+            mode="json"
+            theme="twilight"
+            width="100%"
+            tabSize={2}
+            showPrintMargin={false}
+          />
         </Grid>
-        <AceEditor
-          defaultValue="Loading..."
-          value={this.state.editorValue}
-          onChange={this.handleEditorValueChange}
-          mode="json"
-          theme="twilight"
-          width="100%"
-          tabSize={2}
-          showPrintMargin={false}
-        />
-      </Grid>
+        <Snackbar
+          open={this.state.showSnackbar}
+          autoHideDuration={6000}
+          onClose={this.closeSnackbar}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <SnackbarContent message={this.state.snackbarMessage}/>
+        </Snackbar>
+      </>
     );
   }
 }
