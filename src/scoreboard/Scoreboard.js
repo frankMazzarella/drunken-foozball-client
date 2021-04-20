11f9677a2aa3fbaa18e1af1691a07ee6dfbf2c53
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-04-19 22:45:40 -0400

    hooking up the name yolo to the yayo fax machine over andout

--- src/scoreboard/Scoreboard.js
+++ src/scoreboard/Scoreboard.js
@@ -11,6 +11,7 @@ import {
   Paper,
   makeStyles,
   Grid,
+  TextField,
 } from '@material-ui/core';
 
 import FirebaseService from '../services/Firebase.serivce';
@@ -46,13 +47,21 @@ export default function Scoreboard() {
   const [teamOneScore, setTeamOneScore] = useState('00');
   const [teamTwoScore, setTeamTwoScore] = useState('00');
   const [subtractPoint, setSubtractPoint] = useState(false);
+  const [goalie1Name, setGoalie1Name] = useState('Goalie 1');
+  const [goalie2Name, setGoalie2Name] = useState('Goalie 2');
+  const [striker1Name, setStriker1Name] = useState('Striker 1');
+  const [striker2Name, setStriker2Name] = useState('Striker 2');
   let scoreboardRef = FirebaseService.getDatabaseRef('scoreboard');
 
   useEffect(() => {
     scoreboardRef.on('value', snap => {
-      const scores = snap.val();
-      setScoreboardStats(Object.values(scores));
-      calculateTeamScores(scores);
+      const data = snap.val();
+      setScoreboardStats(Object.values(data));
+      calculateTeamScores(data);
+      setGoalie1Name(data.goalie_one.name);
+      setGoalie2Name(data.goalie_two.name);
+      setStriker1Name(data.striker_one.name);
+      setStriker2Name(data.striker_two.name);
     });
   }, []);
 
@@ -160,17 +169,38 @@ export default function Scoreboard() {
     setSubtractPoint(false);
   }
 
+  const handleGoalie1NameChanged = () => {
+    scoreboardRef.child('goalie_one/name').set(goalie1Name);
+  }
+
+  const handleGoalie2NameChanged = () => {
+    scoreboardRef.child('goalie_two/name').set(goalie2Name);
+  }
+
+  const handleStriker1NameChanged = () => {
+    scoreboardRef.child('striker_one/name').set(striker1Name);
+  }
+
+  const handleStriker2NameChanged = () => {
+    scoreboardRef.child('striker_two/name').set(striker2Name);
+  }
+
   const handleResetGameClicked = () => {
     scoreboardRef.child('goalie_one/goals').set(0);
     scoreboardRef.child('goalie_one/goals_allowed').set(0);
     scoreboardRef.child('goalie_one/own_goals').set(0);
+    scoreboardRef.child('goalie_one/name').set('Goalie 1');
     scoreboardRef.child('striker_one/goals').set(0);
     scoreboardRef.child('striker_one/own_goals').set(0);
+    scoreboardRef.child('striker_one/name').set('Striker 1');
     scoreboardRef.child('goalie_two/goals').set(0);
     scoreboardRef.child('goalie_two/goals_allowed').set(0);
     scoreboardRef.child('goalie_two/own_goals').set(0);
+    scoreboardRef.child('goalie_two/name').set('Goalie 2');
     scoreboardRef.child('striker_two/goals').set(0);
     scoreboardRef.child('striker_two/own_goals').set(0);
+    scoreboardRef.child('striker_two/name').set('Striker 2');
+    setSubtractPoint(false);
   }
 
   const handlePlusMinusClicked = () => {
@@ -185,6 +215,7 @@ export default function Scoreboard() {
   const buildEmailString = () => {
     let str = `
       Game Name:
+      Timestamp: ${new Date().getTime()}
       Team One: ${teamOneScore}
       Team Two: ${teamTwoScore}
     `;
@@ -206,6 +237,70 @@ export default function Scoreboard() {
           <span className={classes.scoreboardText}>{teamOneScore} : {teamTwoScore}</span>
         </div>
       </Grid>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell className={classes.tableHeader}>Player</TableCell>
+                <TableCell className={classes.tableHeader} align="right">G</TableCell>
+                <TableCell className={classes.tableHeader} align="right">GA</TableCell>
+                <TableCell className={classes.tableHeader} align="right">OG</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {scoreboardStats.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">{row.name}</TableCell>
+                  <TableCell align="right">{row.goals}</TableCell>
+                  <TableCell align="right">{row.goals_allowed}</TableCell>
+                  <TableCell align="right">{row.own_goals}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Goalie 1 Name"
+          value={goalie1Name}
+          onChange={event => setGoalie1Name(event.target.value)}
+          onBlur={handleGoalie1NameChanged}
+          variant="filled"
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Striker 1 Name"
+          value={striker1Name}
+          onChange={event => setStriker1Name(event.target.value)}
+          onBlur={handleStriker1NameChanged}
+          variant="filled"
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Goalie 2 Name"
+          value={goalie2Name}
+          onChange={event => setGoalie2Name(event.target.value)}
+          onBlur={handleGoalie2NameChanged}
+          variant="filled"
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          label="Striker 2 Name"
+          value={striker2Name}
+          onChange={event => setStriker2Name(event.target.value)}
+          onBlur={handleStriker2NameChanged}
+          variant="filled"
+          fullWidth
+        />
+      </Grid>
       <Grid item xs={6}>
         <Button
           variant="contained"
@@ -213,8 +308,8 @@ export default function Scoreboard() {
           onClick={handleGoalieOneGoalClicked}
           color="primary">
             {subtractPoint
-              ? "Subtract Goalie 1 Goal"
-              : "Goalie 1 Goal"
+              ? "Subtract " + goalie1Name + " Goal"
+              : goalie1Name + " Goal"
             }
         </Button>
       </Grid>
@@ -225,8 +320,8 @@ export default function Scoreboard() {
           onClick={handleGoalieTwoGoalClicked}
           color="primary">
             {subtractPoint
-              ? "Subtract Goalie 2 Goal"
-              : "Goalie 2 Goal"
+              ? "Subtract " + goalie2Name + " Goal"
+              : goalie2Name + " Goal"
             }
         </Button>
       </Grid>
@@ -237,8 +332,8 @@ export default function Scoreboard() {
           onClick={handleGoalieOneOwnGoalClicked}
           color="default">
             {subtractPoint
-              ? "Subtract Goalie 1 Own Goal"
-              : "Goalie 1 Own Goal"
+              ? "Subtract " + goalie1Name + " Own Goal"
+              : goalie1Name + " Own Goal"
             }
         </Button>
       </Grid>
@@ -249,8 +344,8 @@ export default function Scoreboard() {
           onClick={handleGoalieTwoOwnGoalClicked}
           color="default">
             {subtractPoint
-              ? "Subtract Goalie 2 Own Goal"
-              : "Goalie 2 Own Goal"
+              ? "Subtract " + goalie2Name + " Own Goal"
+              : goalie2Name + " Own Goal"
             }
         </Button>
       </Grid>
@@ -261,8 +356,8 @@ export default function Scoreboard() {
           onClick={handleStrikerOneGoalClicked}
           color="primary">
             {subtractPoint
-              ? "Subtract Striker 1 Goal"
-              : "Striker 1 Goal"
+              ? "Subtract " + striker1Name + " Goal"
+              : striker1Name + " Goal"
             }
         </Button>
       </Grid>
@@ -273,8 +368,8 @@ export default function Scoreboard() {
           onClick={handleStrikerTwoGoalClicked}
           color="primary">
             {subtractPoint
-              ? "Subtract Striker 2 Goal"
-              : "Striker 2 Goal"
+              ? "Subtract " + striker2Name + " Goal"
+              : striker2Name + " Goal"
             }
         </Button>
       </Grid>
@@ -285,8 +380,8 @@ export default function Scoreboard() {
           onClick={handleStrikerOneOwnGoalClicked}
           color="default">
             {subtractPoint
-              ? "Subtract Striker 1 Own Goal"
-              : "Striker 1 Own Goal"
+              ? "Subtract " + striker1Name + " Own Goal"
+              : striker1Name + " Own Goal"
             }
         </Button>
       </Grid>
@@ -297,8 +392,8 @@ export default function Scoreboard() {
           onClick={handleStrikerTwoOwnGoalClicked}
           color="default">
             {subtractPoint
-              ? "Subtract Striker 2 Own Goal"
-              : "Striker 2 Own Goal"
+              ? "Subtract " + striker2Name + " Own Goal"
+              : striker2Name + " Own Goal"
             }
         </Button>
       </Grid>
@@ -328,30 +423,6 @@ export default function Scoreboard() {
           color="default">
             Reset Game
         </Button>
-      </Grid>
-      <Grid item xs={12}>
-        <TableContainer component={Paper}>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell className={classes.tableHeader}>Player</TableCell>
-                <TableCell className={classes.tableHeader} align="right">G</TableCell>
-                <TableCell className={classes.tableHeader} align="right">GA</TableCell>
-                <TableCell className={classes.tableHeader} align="right">OG</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {scoreboardStats.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">{row.name}</TableCell>
-                  <TableCell align="right">{row.goals}</TableCell>
-                  <TableCell align="right">{row.goals_allowed}</TableCell>
-                  <TableCell align="right">{row.own_goals}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
       </Grid>
     </Grid>
   );
